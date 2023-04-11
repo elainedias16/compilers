b83755b2f3d0bfff06e0fcd08356c904f1abcd95
elaindias16 <elainediaspires16@gmail.com>
2023-04-11 11:22:21 -0300

    alguns programas em pascal

---
+++ pascal/Bool.pas
@@ -0,0 +1,13 @@
+program Bool;
+var
+    a, b : boolean;
+
+begin
+    a := true;
+    b := false;
+     
+    writeln( a and b );
+    writeln( a or b );
+    writeln( a xor b );    
+    writeln( not a );  
+end.
